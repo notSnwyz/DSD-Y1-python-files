5265caf4e2675c8d609611eef0b328f416f7fd2c
Mario <mario.fratila081@gmail.com>
2025-11-17 10:37:17 +0000

    Create Task 4.py

---
+++ Lists/Task 4.py
@@ -0,0 +1,7 @@
+notifications = [34, 28, 55, 40, 60, 22, 18]
+
+print(f"Day 1: {notifications[0]}\nDay 2: {notifications[1]}\nDay 3: {notifications[2]}\nDay 4: {notifications[3]}\nDay 5: {notifications[4]}\nDay 6: {notifications[5]}\nDay 7: {notifications[6]}")
+
+print(max(notifications))
+print(min(notifications))
+print(sum(notifications))
