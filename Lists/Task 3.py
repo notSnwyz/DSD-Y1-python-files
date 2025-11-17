689935bdb1eb4898395ba41c476859da85f38318
Mario <mario.fratila081@gmail.com>
2025-11-17 10:24:48 +0000

    Create Task 3.py

---
+++ Lists/Task 3.py
@@ -0,0 +1,7 @@
+listA = [1, 2, 3]
+listB = [1, "2", 3.0] #Python treats mixed-type lists as a string.
+#In a traditional array, all of the values must be the same data type so that they are the same size.
+#Lists are useful because they can handle mixed data types, are easy to understand and are easy to manipulate.
+
+print(listA)
+print(listB)
