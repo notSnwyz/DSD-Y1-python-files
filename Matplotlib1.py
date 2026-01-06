a2e2f5e95b82dff41212ad98416e24a77ebb81b9
Mario <mario.fratila081@gmail.com>
2026-01-06 16:42:32 +0000

    Create Matplotlib1.py

---
+++ Matplotlib1.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+
+# x = [1, 2, 3, 4, 5]
+# y = [2, 4, 6, 8, 10]
+
+# plt.title("Scatter graph")
+# plt.ylabel("Y")
+# plt.xlabel("X")
+# plt.scatter(x, y)
+# plt.show()
+
+# labels = ["Group A", "Group B"]
+# data1 = [10, 20]
+# data2 = [5, 15]
+
+# plt.title("Stacked Bar Graph")
+# plt.ylabel("Y")
+# plt.xlabel("X")
+# plt.bar(labels, data1, color='r')
+# plt.bar(labels, data2, bottom=data1, color='b')
+# plt.show()
+
